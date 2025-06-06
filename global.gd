276ad3fe4b8bab5e62a8b61c829d529d49a85bbf
JellyTop <59908731+scriche@users.noreply.github.com>
2025-06-05 22:18:35 -0700

    lives

--- global.gd
+++ global.gd
@@ -2,9 +2,13 @@ extends Node
 
 var score : int = 0
 var diff : float = 1.0
+var lives : int = 0
 
 func _process(_delta):
-    diff = 1 + Time.get_ticks_msec()/1000.0/300.0
+	diff = 1 + Time.get_ticks_msec()/1000.0/300.0
 
 func set_score(new_score: int) -> void:
-    score = new_score
+	score = new_score
+	
+func set_lives(new_lives: int) -> void:
+	lives = new_lives
